416db3a2ef314a3271b57648713afcadf3ff0f02
Israel Felix <israelfelixbenny@gmail.com>
2023-03-10 10:26:26 -0300

    Service layer, component registration

--- course/src/main/java/com/educandoweb/course/CourseApplication.java
+++ course/src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,7 +1,12 @@
 package com.educandoweb.course;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class CourseApplication {
@@ -9,5 +14,8 @@ public class CourseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
+
+
+	
 
 }
--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -22,8 +22,11 @@ public class TestConfig implements CommandLineRunner {
 	
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User();
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		u3.setId(null);
+		u3.setName("Israel");
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 	}
 
 	
