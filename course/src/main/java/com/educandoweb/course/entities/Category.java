613219f6550cdae47094048b5c21394b0165fedb
Israel Felix <israelfelixbenny@gmail.com>
2023-03-15 10:04:28 -0300

    Criando a entity Product

--- course/src/main/java/com/educandoweb/course/entities/Category.java
+++ course/src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +23,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 
@@ -43,6 +49,10 @@ public class Category implements Serializable {
 		return name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -69,4 +79,7 @@ public class Category implements Serializable {
 		return "Category [id=" + id + ", name=" + name + "]";
 	}
 
+
+
+	
 }
